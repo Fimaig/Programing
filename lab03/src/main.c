a91f5434c06b3de2e44596a167822309fda48437
Fimaig <vullgar13@gmail.com>
2020-10-30 08:44:24 -0400

    doc

--- lab03/src/main.c
+++ lab03/src/main.c
@@ -18,7 +18,7 @@ int num3; //Сотні
 int reverse; //Обернене число
 num1 = VALUE1 % 10;
 num2 = ( VALUE1 % 100 ) / 10;
-num3 = ( VALUE1 % 1000 ) / 100;
+num3 = VALUE1 / 100;
 reverse = num1 * 100 + num2 * 10 + num3;
 
 //Третє завдання
